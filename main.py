f1006ede6b3253abfefe0c54b9ec0a360711c865
navidaminnn <mynameisnavid@gmail.com>
2024-01-07 03:19:22 -0500

    test commit

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import utils.bencoding as bencoding
 from tracker import Tracker
 from metainfo import MetaInfo
-from collections import OrderedDict
 from connection import PeerProtocol, PeerFactory
-from models.piece_status import PieceStatus
-from models.file_writer import FileWriter
 from models.piece import Piece
 from twisted.internet.endpoints import TCP4ClientEndpoint, TCP4ServerEndpoint
 from twisted.internet import reactor
 from twisted.internet.endpoints import connectProtocol
-import os
 
-def start_server(tracker: Tracker, meta_info: MetaInfo, piece_status: PieceStatus, file_writer: FileWriter):
+def start_server(tracker: Tracker, meta_info: MetaInfo):
     endpoint = TCP4ServerEndpoint(reactor, 8000)
-    factory = PeerFactory(tracker.peers, meta_info.info_hash, tracker.peer_id, piece_status, file_writer)
+    factory = PeerFactory(tracker.peers, meta_info, tracker.peer_id)
     endpoint.listen(factory)
 
     for peer in tracker.peers:
@@ -34,15 +30,12 @@ if __name__ == "__main__":
 
     tracker = Tracker(meta_info)
     tracker.fetch_peers()
-    
-    piece_status = PieceStatus(tracker.peers, meta_info)
-
-    file_writer = FileWriter(meta_info, piece_status)
 
     print(f"Number of pieces: {meta_info.num_pieces}")
     print(f"Number of peers: {len(tracker.peers)}")
     print(f"Piece length: {meta_info.piece_length}")
+    print("Total # of bytes: %d" % meta_info.length)
 
-    start_server(tracker, meta_info, piece_status, file_writer)
+    start_server(tracker, meta_info)
 
     reactor.run()
